e000cc478be72321c380be0d4a863d549b214fb2
hzh0603 <1165934120@qq.com>
2021-07-06 09:37:23 +0800

    feat: 组件库按需引入处理

--- packages/vue-cli-plugin-ui-template/generator/index.js
+++ packages/vue-cli-plugin-ui-template/generator/index.js
@@ -1,7 +1,8 @@
+const fs = require('fs')
 module.exports = (api) => {
     let renderPage = {};
     // 获取选择的UI组件类型
-    const { ui } = api.options
+    const { ui, importStyle } = api.options
     let dependencies = {}
     if(ui === 'element') {
         dependencies['element-ui'] = '2.15.3'
@@ -10,10 +11,32 @@ module.exports = (api) => {
         dependencies['ant-design-vue'] = '1.7.6'
         renderPage['./src/plugins/antdv.js'] = './template/src/plugins/antdv.js'
     }
+    if(importStyle === 'part') {
+        dependencies['babel-plugin-component'] = '^1.1.1'
+    }
     api.extendPackage({
         dependencies
     })
     api.render(renderPage, api.options);
 
     api.injectImports('src/main.ts', `import './plugins/${ui}.js'`)
+
+    api.onCreateComplete(() => {
+        // 局部引用处理按需加载
+        if(importStyle === 'part') {
+            let pluginComponent = [];
+            if(ui === 'Element') {
+                pluginComponent = ['component', { 'libraryName': 'element-ui', 'styleLibraryName': 'theme-chalk'}] 
+            } else {
+                pluginComponent = ["import", { "libraryName": "ant-design-vue", "libraryDirectory": "es", "style": "css" }]
+            }
+            const rcPath = api.resolve('./babel.config.js')
+            const bableConfig = require(rcPath)
+            // 配置添加按需加载配置
+            bableConfig.plugins = bableConfig.plugins || []
+            bableConfig.plugins.push(pluginComponent)
+            // 重新写入babel.config.js
+            fs.writeFileSync(rcPath, JSON.stringify(bableConfig, null, 2), {encoding: 'utf8'})
+        }
+    })
 }
